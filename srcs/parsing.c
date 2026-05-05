043999ae7bd5d4aee6419d1b9c3eee417265eaec
Noah Felten <noahfelten@Noahs-MacBook-Pro.local>
2026-05-05 11:45:22 +0200

    norm parsing.c

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -12,29 +12,29 @@
 
 #include "../includes/cub3d.h"
 
-int	checkFileExtension(char *fileName)
+int	check_file_extension(char *file_name)
 {
-	char	*fileExtension;
+	char	*file_extension;
 	size_t	i;
 	size_t	j;
 
-	if (!fileName || !fileName[0])
+	if (!file_name || !file_name[0])
 		return (1);
-	i = ft_strlen(fileName + 1);
+	i = ft_strlen(file_name + 1);
 	j = 3;
-	fileExtension = malloc(sizeof(char *) * 5);
-	fileExtension[0] = '.';
-	fileExtension[1] = 'c';
-	fileExtension[2] = 'u';
-	fileExtension[3] = 'b';
-	fileExtension[4] = '\0';
+	file_extension = malloc(sizeof(char *) * 5);
+	file_extension[0] = '.';
+	file_extension[1] = 'c';
+	file_extension[2] = 'u';
+	file_extension[3] = 'b';
+	file_extension[4] = '\0';
 	while (j > 0)
 	{
-		if (fileName[i] != fileExtension[j])
-			return (free(fileExtension), 1);
+		if (file_name[i] != file_extension[j])
+			return (free(file_extension), 1);
 		i--;
 		j--;
 	}
-	free(fileExtension);
+	free(file_extension);
 	return (0);
 }
